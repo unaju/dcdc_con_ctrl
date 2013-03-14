20e9942e6c5f70553d090e0968cc403a65160be4
xll(laptop) <xirstar+git@gmail.com>
2013-03-14 15:57:11 +0900

    abs追加

--- dcdc_con_ctrl/alg.hpp
+++ dcdc_con_ctrl/alg.hpp
@@ -2,9 +2,10 @@
 
 namespace alg
 {
-	template<typename T>	T min(T a,  T b) { return (a < b) ? a : b; }
-	template<typename T>	T max(T a,  T b) { return (a > b) ? a : b; }
+	template<typename T>	T min(T a, T b) { return (a < b) ? a : b; }
+	template<typename T>	T max(T a, T b) { return (a > b) ? a : b; }
 
+	template<typename T>	T abs(T val) { return (val > 0) ? val : -val; } 
 };
 
 
